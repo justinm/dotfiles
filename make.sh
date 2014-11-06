3bddc2f31fdd6c92e2a4f35ee9ba60336a09bbe9
Justin McCormick <me@justinmccormick.com>
2014-11-06 00:55:10 -0600

    Adding .bashrc support

--- make.sh
+++ make.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 installSymlink() {
 	([ -f "$2" ] || [ -d "$2" ]) && mv "$2" "$2.orig"
 	[ ! -e "$2" ] && ln -s "$1" "$2"
@@ -14,11 +16,14 @@ uninstallSymlink() {
 if [ "$1" = "install" ]; then
 	installSymlink $PWD/.vimrc ~/.vimrc
 	installSymlink $PWD/.vim ~/.vim
+
+	if ! grep -q "dotfiles/.bashrc" ~/.bashrc; then
+		echo "\n[ -e $DIR/.bashrc ] && source $DIR/.bashrc\n" >> ~/.bashrc
+	fi
 fi
 
 if [ "$1" = "uninstall" ]; then
 	uninstallSymlink ~/.vimrc
 	uninstallSymlink ~/.vim
 fi
-
 
