32ba253ff0dd546d61bfcddc6849a6c9e9bb08ba
Justin McCormick <me@justinmccormick.com>
2023-08-21 10:24:10 -0400

    asdf -> rtx

--- activate.zsh
+++ activate.zsh
@@ -15,21 +15,14 @@
 # Uncomment the following line to enable command auto-correction.
 # ENABLE_CORRECTION="true"
 
-ENABLE_ANTIGEN=true
-
-if [[ $__INTELLIJ_COMMAND_HISTFILE__ ]]; then
-  # Remove the user@computer from prompt
-  export DEFAULT_USER=$USER
-  THEME="robbyrussell"
-else
-  THEME=romkatv/powerlevel10k
-fi
+ENABLE_JAVA="false"
+ENABLE_NVM="false"
 
 DOTFILE_PATH=$(dirname $0:A)
 
 # Compilation flags
-export EDITOR='vim'
 #export ARCHFLAGS="-arch x86_64"
+export EDITOR='vim'
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
@@ -37,65 +30,66 @@ fpath+=$DOTFILE_PATH/completions
 
 ##################################
 
-if [[ "$ENABLE_ANTIGEN" == "true" ]]; then
-    HIST_STAMPS="yyyy/mm/dd"
-    COMPLETION_WAITING_DOTS="true"
+THEME="robbyrussell"
 
-    source $DOTFILE_PATH/vendor/antigen.zsh
+# powerlevel10k tab complete is odd in IntelliJ's terminal
+#[[ "$__CFBundleIdentifier" != "com.jetbrains.intellij" ]] && [[ -f $DOTFILE_PATH/.p10k.zsh ]] && {
+  THEME="romkatv/powerlevel10k"
 
-    # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-    [[ "$THEME" == "romkatv/powerlevel10k" ]] && [[ -f $DOTFILE_PATH/.p10k.zsh ]] && source $DOTFILE_PATH/.p10k.zsh
+  source $DOTFILE_PATH/.p10k.zsh
+#}
 
-    antigen use oh-my-zsh
+# Antigen
+##################################
+source $DOTFILE_PATH/vendor/antigen.zsh
 
-    antigen bundle aws
-    antigen bundle brew
-    antigen bundle brew-cask
-    antigen bundle command-not-found
-    antigen bundle git
-    antigen bundle pip
-    antigen bundle fzf
-    antigen bundle mroth/evalcache
-    antigen bundle reegnz/jq-zsh-plugin
-    antigen bundle joshskidmore/zsh-fzf-history-search  
-    antigen bundle zsh-users/zsh-autosuggestions
-    antigen bundle zsh-users/zsh-completions
-    antigen bundle zsh-users/zsh-history-substring-search
-    antigen bundle zsh-users/zsh-syntax-highlighting
+# Remove the user@computer from prompt
+export DEFAULT_USER=$USER
+HIST_STAMPS="yyyy/mm/dd"
+COMPLETION_WAITING_DOTS="true"
 
-    # Load the theme.
-    if [[ ! -z "$THEME" ]] && antigen theme $THEME
-fi
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 
-export ZSH_EVALCACHE_DIR="$HOME/.zsh/cache"
-export KREW_ROOT=${KREW_ROOT:-$HOME/.krew}
-export PATH="/usr/local/bin:$PATH"
-export PATH="$HOME/bin:$PATH"
-export PATH="$PATH:${KREW_ROOT:-$HOME/.krew}/bin"
-export PATH="$PATH:Applications/Postgres.app/Contents/Versions/latest/bin"
-export PATH="$PATH:${KREW_ROOT}/bin"
+antigen use oh-my-zsh
 
-if [[ -e "$HOME/.nvm" ]]; then
+antigen bundle aws
+antigen bundle brew
+antigen bundle brew-cask
+antigen bundle command-not-found
+antigen bundle git
+antigen bundle pip
+antigen bundle fzf
+antigen bundle mroth/evalcache
+antigen bundle reegnz/jq-zsh-plugin
+#antigen bundle joshskidmore/zsh-fzf-history-search
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-history-substring-search
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+antigen theme $THEME
+
+[[ "$ENABLE_NVM" == "true" ]] && [[ -e "$HOME/.nvm" ]] && {
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-    [[ "$ENABLE_ANTIGEN" == "true" ]] && antigen bundle nvm
-fi
+    antigen bundle nvm
+}
 
 # Tell Antigen that you're done.
-[[ "$ENABLE_ANTIGEN" == "true" ]] && antigen apply
+antigen apply
 
-export KREW_ROOT=${KREW_ROOT:-$HOME/.krew}
-export PATH="/usr/local/bin:$PATH"
-export PATH="$HOME/bin:$PATH"
-export PATH="$PATH:${KREW_ROOT:-$HOME/.krew}/bin"
-export PATH="$PATH:Applications/Postgres.app/Contents/Versions/latest/bin"
-export PATH="$PATH:${KREW_ROOT}/bin"
+export ZSH_EVALCACHE_DIR="$HOME/cache/eval-cache"
 
-# if [ -e /usr/libexec/java_home ]; then
-# 	_evalcache export JAVA_HOME=$(/usr/libexec/java_home)
-# fi
+[[ "$ENABLE_JAVA" == "true" ]] && [[ -e "/usr/libexec/java_home" ]] && {
+  JAVA_HOME=$(/usr/libexec/java_home)
+  export JAVA_HOME
+}
+
+if [[ -e "$HOME/bin/rtx" ]]; then
+  eval "$(~/bin/rtx activate zsh)"
+fi
 
 if [[ -e "/opt/homebrew/opt/asdf/libexec/asdf.sh" ]]; then
     . /opt/homebrew/opt/asdf/libexec/asdf.sh
@@ -133,3 +127,11 @@ zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh/cache
 
 setopt rm_star_silent
+
+export KREW_ROOT=${KREW_ROOT:-$HOME/.krew}
+export PATH="/usr/local/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
+export PATH="$PATH:${KREW_ROOT:-$HOME/.krew}/bin"
+export PATH="$PATH:Applications/Postgres.app/Contents/Versions/latest/bin"
+export PATH="$PATH:${KREW_ROOT}/bin"
+export DOCKER_DEFAULT_PLATFORM="linux/amd64"
